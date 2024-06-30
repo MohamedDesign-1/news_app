14d8b95060ea5cee169bab7590c079a7163dbe7c
m7med <m7medyt2017@gmail.com>
2024-07-01 02:09:02 +0300

    HomeScreen is done

--- lib/widgets/category_cards.dart
+++ lib/widgets/category_cards.dart
@@ -3,29 +3,31 @@ import 'package:newsapp/models/category_model.dart';
 
 class CategoryCards extends StatelessWidget {
   CategoryCards({Key? key, required this.category}) : super(key: key);
-  final CategoryModel category ;
+  final CategoryModel category;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.all(8),
+      margin: EdgeInsets.only(right: 8),
       height: 120,
       width: 200,
       decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(4),
+          borderRadius: BorderRadius.circular(8),
           color: Colors.orange,
           image: DecorationImage(
               image: AssetImage(category.imageAssetsUrl),
               fit: BoxFit.fill,
-              colorFilter: ColorFilter.mode(Colors.black87 , BlendMode.screen)
-          )
-      ),
-      child:Center(
-        child: Text( category.categoryName , style: TextStyle(
-          color: Colors.white ,
-          fontSize:  20,
-          fontWeight: FontWeight.bold,
-        ),),
+              colorFilter: ColorFilter.mode(
+                  Colors.black.withOpacity(0.4), BlendMode.multiply))),
+      child: Center(
+        child: Text(
+          category.categoryName,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
       ),
     );
   }
